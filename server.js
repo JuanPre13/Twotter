a39a017ccc4a2fb42079932a8bf96f98ab7bde47
Juan <prendas52@gmail.com>
2020-12-21 12:11:43 -0600

    Server configuration fixed

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const serveStatic = require('server-static');
+const serveStatic = require('serve-static');
 const history = require('connect-history-api-fallback');
 const enforce = require('express-sslify');
 
 const app = express();
 
 app.use(enforce.HTTPS({ trustProtoHeader: true }));
-app.user(serveStatic(__dirname + '/dist'));
+app.use(serveStatic(__dirname + '/dist'));
 app.use(history());
 
-app.listen(process.env.PORT || 5000)
+app.listen(process.env.PORT || 5000);
